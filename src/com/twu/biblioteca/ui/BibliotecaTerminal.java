a003a0642144b57fcfafdcc79ecaf84dc66fe3b2
othman853 <anuar.yasser@gmail.com>
2016-01-07 18:45:03 -0200

    Refactoring terminal to use Menu

--- src/com/twu/biblioteca/ui/BibliotecaTerminal.java
+++ src/com/twu/biblioteca/ui/BibliotecaTerminal.java
@@ -1,10 +1,12 @@
 package com.twu.biblioteca.ui;
 
+import com.twu.biblioteca.ui.menu.Menu;
 import java.util.List;
 
 public class BibliotecaTerminal implements Terminal {
 
     public static final String GREETING = "Hello! Welcome to Biblioteca!";
+    private Menu menu;
 
     @Override
     public void greet() {
@@ -16,6 +18,11 @@ public class BibliotecaTerminal implements Terminal {
         for (Object element : elementsToDisplay) {
             System.out.println(element.toString());
         }
+    }
+
+    @Override
+    public Menu getMenu() {
+        return menu;
     }
 
 }
--- src/com/twu/biblioteca/ui/Terminal.java
+++ src/com/twu/biblioteca/ui/Terminal.java
@@ -1,11 +1,13 @@
 package com.twu.biblioteca.ui;
 
 
+import com.twu.biblioteca.ui.menu.Menu;
 import java.util.List;
 
 public interface Terminal {
 
     void greet();
     void printList(List elementsToDisplay);
+    Menu getMenu();
 
 }
