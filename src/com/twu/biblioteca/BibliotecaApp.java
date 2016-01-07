adca6c003af91de89e836ee3dcf045a1ccad32f9
othman853 <anuar.yasser@gmail.com>
2016-01-07 15:29:09 -0200

    Refactoring app to print list

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,19 +1,25 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.service.BookService;
 import com.twu.biblioteca.ui.Terminal;
+
+import com.twu.biblioteca.model.Book;
 
 public class BibliotecaApp {
 
     public static final String GREETING = "Hello! Welcome to Biblioteca";
 
     private Terminal terminal;
+    private BookService<Book> service;
 
-    public BibliotecaApp(Terminal terminal) {
+    public BibliotecaApp(Terminal terminal, BookService<Book> service) {
         this.terminal = terminal;
+        this.service = service;
     }
 
     public void run () {
         terminal.greet();
+        terminal.printList(service.getAll());
     }
 
 }
