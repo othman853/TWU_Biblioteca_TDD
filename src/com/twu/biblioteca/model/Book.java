213df9dcf60b7d5c074cdae4b7d03ad1b62b4a47
othman853 <anuar.yasser@gmail.com>
2016-01-07 15:28:16 -0200

    Refactoring toString implementation

--- src/com/twu/biblioteca/model/Book.java
+++ src/com/twu/biblioteca/model/Book.java
@@ -9,15 +9,20 @@ public class Book {
     private boolean available;
     private int yearPublished;
 
+    private String displayFormat;
+
     public Book(int code, String name, String authors, boolean available, int yearPublished) {
         this.code = code;
         this.name = name;
         this.authors = authors;
         this.available = available;
         this.yearPublished = yearPublished;
+
+        displayFormat = "[code]: [name] by [author]. Published in [year]";
     }
 
     public Book() {
+        displayFormat = "[code]: [name] by [authors]. Published in [yearPublished]";
     }
 
     public int getCode() {
@@ -58,5 +63,20 @@ public class Book {
 
     public void setYearPublished(int yearPublished) {
         this.yearPublished = yearPublished;
+    }
+
+    public String getDisplayFormat() {
+        return displayFormat;
+    }
+
+    @Override
+    public String toString() {
+
+        return displayFormat
+                .replace("[code]", Integer.toString(code))
+                .replace("[name]", name)
+                .replace("[authors]", authors)
+                .replace("[yearPublished]", Integer.toString(yearPublished));
+
     }
 }
